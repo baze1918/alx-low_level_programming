4f99918e5261bc19c72805ada69d5e8e5a63f34b
bazezew <bazezewbelew@gmail.com>
2023-02-18 06:19:00 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - Entry pint is main function of the program or the code
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
