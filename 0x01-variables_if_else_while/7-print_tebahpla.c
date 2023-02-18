5ea887d7442ca6619666830b9dcbf6e57635f887
bazezew <bazezewbelew@gmail.com>
2023-02-17 18:28:47 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - Entrty pint of the funchion
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+c = 'z';
+while
+(c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
