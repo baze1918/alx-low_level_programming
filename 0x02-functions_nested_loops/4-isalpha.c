a7bbcf8d5665aa99683eb8f8527a1d36917f43e5
bazezew <bazezewbelew@gmail.com>
2023-02-21 14:57:07 -0800

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alpha character
+ * @c: The character to be checked
+ * Return: 1 for alpha character or 0 for anything else
+ */
+int _islower(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
