893d221dc6ca70e723453ec7be1fbee04e33c8b5
bazezew <bazezewbelew@gmail.com>
2023-02-21 16:14:20 -0800

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - checks for sign character
+ * @x: The character to be checked
+ * Return: x, -x
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (-x);
+	}
+}
