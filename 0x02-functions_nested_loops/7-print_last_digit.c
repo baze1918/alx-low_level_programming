4adcda33c626cd364134b686a378a2499cedb128
bazezew <bazezewbelew@gmail.com>
2023-02-21 16:37:19 -0800

    7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - last values
+ * @n: The character to be checked
+ * Return:  last
+ */
+int print_last_digit(int n);
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+		last * (-1);
+	}
+		_putchar(last + '0');
+		return (last);
+	}
+}
