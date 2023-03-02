32c139c2c2e2db4a01f35c4c4d81942a840cebf6
bazezew <bazezewbelew@gmail.com>
2023-03-02 12:47:47 -0800

    1-last_digi

--- 0x01-variables_if_else_while/1-last_digi.c
+++ 0x01-variables_if_else_while/1-last_digi.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
+/**
+* main - prints a text according number
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+	int n, lastd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("last digit of %d is %d and is greater than 5\n", n, lasted);
+
+	}
+	else if (lastd == 0)
+	{
+		printf("last digit of %d is %d and is  0\n", n, lasted);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("last digit of %d is %d and is  0\n", n, lasted);
+	}
+	return (0);
+}
