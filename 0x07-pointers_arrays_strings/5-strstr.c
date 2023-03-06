a0ab87dc8f11ec96a0f3c343f4320549f43fa91a
bazezew <bazezewbelew@gmail.com>
2023-03-06 10:11:57 -0800

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,10 +10,11 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	for(; *haystack != '\0'; haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
 	char *one = haystack;
 	char *two = needle;
+
 	while (*one == *two && *two != '\0')
 		{
 		one++;
