8a7141340f1e7c92db0e13ac471504732c7ab62f
bazezew <bazezewbelew@gmail.com>
2023-03-13 06:13:16 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	aa = malloc(sizeof(char)*(i + 1));
+	aa = malloc(sizeof(char) * (i + 1));
 	if (aa == NULL)
 		return (NULL);
 	for (r = 0; str[r]; r++)
